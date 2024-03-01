8d020df5519bdbcd11e983586c9b2ad995a1e59b
usuario1 <usuario@gmail.com>
2024-03-01 14:24:03 +0100

    aaaaaa

---
+++ src/components/consume.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "consume",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: () => import("./components/HelloWorld.vue"),
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: () => import("./components/consume.vue"),
+    },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+
+createApp(App).mount("#app");
